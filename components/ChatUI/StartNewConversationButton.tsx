21c098d3a3b6565190609af21059b0e23268e62e
Kurasao <jamerolenor@gmail.com>
2025-03-09 16:45:29 +0800

    Refactor CarRepairDataset, ConversatioFlow and improve StartNewConversationButton layout

--- components/ChatUI/StartNewConversationButton.tsx
+++ components/ChatUI/StartNewConversationButton.tsx
@@ -9,10 +9,11 @@ type StartNewConversationButtonProps = {
 const StartNewConversationButton: React.FC<StartNewConversationButtonProps> = ({ onPress }) => (
   <TouchableOpacity
     onPress={onPress}
-    className="bg-purple-500 px-4 py-2 rounded-lg mb-2 items-center"
+    className="flex flex-row justify-center bg-purple-500 px-4 py-2 rounded-lg mb-2 items-center"
   >
-    <Text className="text-white">
-      <MaterialIcons name="refresh" size={16} color="white" /> Start New Conversation
+    <MaterialIcons name="refresh" size={16} color="white" /> 
+    <Text className="text-white text-xl ml-2">
+    Start New Conversation
     </Text>
   </TouchableOpacity>
 );
